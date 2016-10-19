dfa887dbe5cc09db3d67e7b540815c968ee9666f
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-19 17:06:55 +0700

    index

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const app = express()
+const fs = require("fs")
+const multer = require("multer")
+const upload = multer({ dest: "./uploads" })
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/images")
+const conn = mongoose.connection
+
+const Grid = require("gridfs-stream")
+Grid.mongo = mongoose.mongo
+var gfs
+
+conn.once("open", function () {
+  gfs = Grid(conn.db)
+
+  app.get("/", function (req, res) {
+    // renders a multipart/form-data form
+    res.render("home")
+  })
+
+  // second parameter is multer middleware.
+  app.post("/", upload.single("avatar"), function (req, res, next) {
+    //create a gridfs-stream into which we pipe multer's temporary file saved in uploads. After which we delete multer's temp file.
+    var writestream = gfs.createWriteStream({
+      filename: req.file.originalname
+    })
+
+    // pipe multer's temp file /uploads/filename into the stream we created above. On end deletes the temporary file.
+    fs.createReadStream("./uploads/" + req.file.filename)
+      .on("end", function () { fs.unlink("./uploads/" + req.file.filename, function (err) { res.send("success") }) })
+      .on("err", function () { res.send("Error uploading image") })
+      .pipe(writestream)
+  })
+
+  // sends the image we saved by filename
+  app.get("/files/:filename", function (req, res) {
+    var readstream = gfs.createReadStream({ filename: req.params.filename })
+    readstream.on("error", function (err) {
+      res.send("No image found with that title")
+    })
+    readstream.pipe(res)
+  })
+
+  // delete the image
+  app.get("/delete/:filename", function (req, res) {
+    gfs.exist({ filename: req.params.filename }, function (err, found) {
+      if (err) return res.send("Error occured")
+      if (found) {
+        gfs.remove({ filename: req.params.filename }, function (err) {
+          if (err) return res.send("Error occured")
+          res.send("Image deleted!")
+        })
+      } else {
+        res.send("No image found with that title")
+      }
+    })
+  })
+})
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
+
+if (!module.parent) {
+  app.listen(3000)
+}
