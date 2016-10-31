04d83c7a69e3c1e190c5a5fcb79f9d83c1972d69
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-31 17:06:41 +0700

    server + multer disk

---
+++ multer-jquery/server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const multer = require('multer')
+const app = express()
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads')
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+const upload = multer({ storage: storage }).single('userPhoto')
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post('/api/photo', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end("Error uploading file.")
+    }
+    res.end("File is uploaded!")
+  })
+})
+
+app.listen(3000, function () {
+  console.log("Server on localhost:3000")
+})
